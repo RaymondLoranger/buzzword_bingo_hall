db21dd5dd37fe16ecda1ce0ced0644ac9a2b8fa1
RaymondLoranger <Raymond.Loranger@bell.net>
2021-02-04 13:27:41 -0500

    0.1.1

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,6 +14,7 @@ import "../css/app.scss"
 //     import socket from "./socket"
 //
 import "phoenix_html"
+import 'bootstrap' // Bootstrap support
 import './copy-game-url'
 import './color-selector'
 import './vue-game'
