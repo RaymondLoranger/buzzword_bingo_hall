8da61b75e8c8116ff5768ecc3193583712f7328b
RaymondLoranger <Raymond.Loranger@bell.net>
2021-02-06 21:38:54 -0500

    0.1.8

--- test/buzzword_bingo_hall_web/controllers/game_controller_test.exs
+++ test/buzzword_bingo_hall_web/controllers/game_controller_test.exs
@@ -6,15 +6,12 @@ defmodule Buzzword.Bingo.HallWeb.GameControllerTest do
   describe "new" do
     test "redirects to new session if not authenticated", %{conn: conn} do
       conn = get(conn, Routes.game_path(conn, :new))
-
       assert redirected_to(conn) == Routes.session_path(conn, :new)
     end
 
     test "renders new game form if authenticated", %{conn: conn} do
       conn = put_player_in_session(conn, "mike")
-
       conn = get(conn, Routes.game_path(conn, :new))
-
       assert html_response(conn, 200) =~ "Start Game"
     end
   end
@@ -22,18 +19,14 @@ defmodule Buzzword.Bingo.HallWeb.GameControllerTest do
   describe "create" do
     test "redirects to new session if not authenticated", %{conn: conn} do
       conn = post(conn, Routes.game_path(conn, :create))
-
       assert redirected_to(conn) == Routes.session_path(conn, :new)
     end
 
     test "redirects to show game if authenticated", %{conn: conn} do
       conn = put_player_in_session(conn, "mike")
-
       conn = post(conn, Routes.game_path(conn, :create), game: %{"size" => "3"})
-
       assert %{id: game_name} = redirected_params(conn)
       assert redirected_to(conn) == Routes.game_path(conn, :show, game_name)
-
       conn = get(conn, Routes.game_path(conn, :show, game_name))
       assert html_response(conn, 200) =~ "game-container"
     end
@@ -42,25 +35,19 @@ defmodule Buzzword.Bingo.HallWeb.GameControllerTest do
   describe "show" do
     test "redirects to new session if not authenticated", %{conn: conn} do
       conn = get(conn, Routes.game_path(conn, :show, "123"))
-
       assert redirected_to(conn) == Routes.session_path(conn, :new)
     end
 
     test "redirects to new game if game does not exist", %{conn: conn} do
       conn = put_player_in_session(conn, "mike")
-
       conn = get(conn, Routes.game_path(conn, :show, "nonexistent-game"))
-
       assert redirected_to(conn) == Routes.game_path(conn, :new)
     end
 
     test "shows game if it exists", %{conn: conn} do
       conn = put_player_in_session(conn, "mike")
-
       game_name = start_game()
-
       conn = get(conn, Routes.game_path(conn, :show, game_name))
-
       assert html_response(conn, 200) =~ game_name
     end
   end
