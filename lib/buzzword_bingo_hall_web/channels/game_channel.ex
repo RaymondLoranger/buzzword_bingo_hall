6a99403efffc154ce1e115ddf4452afa686e2019
RaymondLoranger <Raymond.Loranger@bell.net>
2021-02-08 06:31:24 -0500

    0.1.12

--- lib/buzzword_bingo_hall_web/channels/game_channel.ex
+++ lib/buzzword_bingo_hall_web/channels/game_channel.ex
@@ -16,7 +16,7 @@ defmodule Buzzword.Bingo.HallWeb.GameChannel do
   end
 
   def handle_info({:after_join, game_name}, socket) do
-    %Summary{} = summary = Engine.game_summary(game_name)
+    summary = %Summary{} = Engine.game_summary(game_name)
     :ok = push(socket, "game_summary", summary)
     :ok = push(socket, "presence_state", Presence.list(socket))
 
@@ -35,8 +35,8 @@ defmodule Buzzword.Bingo.HallWeb.GameChannel do
 
     case Engine.game_pid(game_name) do
       pid when is_pid(pid) ->
-        %Summary{} =
-          summary =
+        summary =
+          %Summary{} =
           Engine.mark_square(game_name, phrase, current_player(socket))
 
         broadcast!(socket, "game_summary", summary)
